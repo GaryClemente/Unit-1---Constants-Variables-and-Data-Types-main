4835b7e21a45459f957e3c055851902abcaaa275
Gary Clemente <gary.clemente07@gmail.com>
2025-09-09 09:28:33 -0400

    jhjhhbkjkjh

--- Lab - Constants and Variables.playground/Pages/9. Exercise - Type Inference and Required Values.xcplaygroundpage/Contents.swift
+++ Lab - Constants and Variables.playground/Pages/9. Exercise - Type Inference and Required Values.xcplaygroundpage/Contents.swift
@@ -4,16 +4,17 @@
  Declare a variable called `name` of type `String`, but do not give it a value. Print `name` to the console. Does the code compile? Remove any code that will not compile.
  */
 
-
 //:  Now assign a value to `name`, and print it to the console.
-
+var name = ""
+print(name)
 
 //:  Declare a variable called `distanceTraveled` and set it to 0. Do not give it an explicit type.
+var distanceTraveled = 0
 
 
 //:  Now assign a value of 54.3 to `distanceTraveled`. Does the code compile? Go back and set an explicit type on `distanceTraveled` so the code will compile.
-
-
+//distanceTraveled += 54.3
+print("it will not work since it says int can not be double")
 /*:
 [Previous](@previous)  |  page 9 of 10  |  [Next: App Exercise - Percent Completed](@next)
  */
